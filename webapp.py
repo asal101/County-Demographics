214de39525e07ec0a494d2f51cd8281696d9f1dd
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-18 10:21:12 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,54 +5,12 @@ app = Flask(__name__)
 
 
 get_state_options
-  Alabama 
-  Alaska 
-  Arizona 
-  Arkansas 
-  California 
-  Colorado 
-  Connecticut 
-  Delaware 
-  Florida 
-  Georgia 
-  Hawaii 
-  Idaho 
-  Illinois Indiana 
-  Iowa 
-  Kansas 
-  Kentucky 
-  Louisiana 
-  Maine 
-  Maryland 
-  Massachusetts 
-  Michigan 
-  Minnesota 
-  Mississippi 
-  Missouri 
-  Montana Nebraska 
-  Nevada 
-  New Hampshire 
-  New Jersey 
-  New Mexico 
-  New York 
-  North Carolina 
-  North Dakota 
-  Ohio 
-  Oklahoma 
-  Oregon 
-  Pennsylvania Rhode Island 
-  South Carolina 
-  South Dakota 
-  Tennessee 
-  Texas 
-  Utah 
-  Vermont 
-  Virginia 
-  Washington 
-  West Virginia 
-  Wisconsin 
-  Wyoming
-
-options = options + Markup("<get_state_options=\"" + s + "\get_state_options">" + s + "</get_state_options>")
+  
+  "empty_list": "",
+    
+   Markup("<empty_list=\"" + s + "\">" + s + "</empty_list>")
+  
+  
+options = options + Markup("<empty_list=\"" + s + "\get_state_options">" + s + "</empty_list>")
 
 
