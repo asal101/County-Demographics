f57bbfaee58ced590c81d3db1bf9e7055128a627
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-18 10:08:12 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,3 +2,57 @@ from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
 app = Flask(__name__)
+
+
+get_state_options
+  Alabama 
+  Alaska 
+  Arizona 
+  Arkansas 
+  California 
+  Colorado 
+  Connecticut 
+  Delaware 
+  Florida 
+  Georgia 
+  Hawaii 
+  Idaho 
+  Illinois Indiana 
+  Iowa 
+  Kansas 
+  Kentucky 
+  Louisiana 
+  Maine 
+  Maryland 
+  Massachusetts 
+  Michigan 
+  Minnesota 
+  Mississippi 
+  Missouri 
+  Montana Nebraska 
+  Nevada 
+  New Hampshire 
+  New Jersey 
+  New Mexico 
+  New York 
+  North Carolina 
+  North Dakota 
+  Ohio 
+  Oklahoma 
+  Oregon 
+  Pennsylvania Rhode Island 
+  South Carolina 
+  South Dakota 
+  Tennessee 
+  Texas 
+  Utah 
+  Vermont 
+  Virginia 
+  Washington 
+  West Virginia 
+  Wisconsin 
+  Wyoming
+
+options = options + Markup("<get_state_options=\"" + s + "\get_state_options">" + s + "</get_state_options>")
+
+
