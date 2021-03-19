908bbd953d04f3017798087d852122162698942a
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-19 10:15:33 -0700

    Create webapp.py

--- webapp.py
+++ webapp.py
@@ -10,7 +10,9 @@ def render_main():
 @app.route("/response")
 def render_response():
     color = request.args['color'] 
-    
+    #The request object stores information about the request sent to the server.
+    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
+    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
     if color == 'pink':
         reply = "That's my favorite color, too!"
     else:
@@ -19,10 +21,31 @@ def render_response():
     
 if __name__=="__main__":
     app.run(debug=False, port=54321)
-    get_state_options:
-      ListOfStates:
+
+   
+  def get_state_options():
+        
+      ListOfStates=[]
         options = options + [0] "State"
         for state in states:
         if state["State"]  < first_state:
             first_state = state ["State"]
        return first_state     
+
+Markup("<State=\"" + s + "\">" + s + "</State>")
+
+
+
+
+
+
+
+
+
+       
+        
+    
+    
+    
+    
+    
