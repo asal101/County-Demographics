f5bc0c0beac718b611202c449e89b12483f6efb3
asal101 <77691090+asal101@users.noreply.github.com>
2021-03-19 10:02:12 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,14 +9,12 @@ def render_main():
                            
 @app.route("/response")
 def render_response():
-    color = request.args['color'] 
-    #The request object stores information about the request sent to the server.
-    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
-    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
+    #color = request.args['color'] 
+    
+   # if color == 'pink':
         reply = "That's my favorite color, too!"
-    else:
-        reply = "My favorite color is pink."
+  #  else:
+   #     reply = "My favorite color is pink."
     return render_template('response.html', response = reply)
     
 if __name__=="__main__":
