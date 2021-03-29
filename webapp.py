15ff041486b82923ddf2eb66ced5b641cc5aa575
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-29 09:42:34 -0700

    Create webapp.py

--- webapp.py
+++ webapp.py
@@ -4,49 +4,30 @@ import json
 app = Flask(__name__)
 
 @app.route("/")
-def render_main():
-    return render_template('home.html', options=get_state_options())
+def home():
+    return render_template('home.html')
                            
-@app.route("/response")
-def render_response():
-    
-    
-   # color = request.args['color'] 
-    
-    
-    #The request object stores information about the request sent to the server.
-    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
-    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    
-    
-   # if color == 'pink':
-   #     reply = "That's my favorite color, too!"
-  #  else:
-  #      reply = "My favorite color is pink."
-  #  return render_template('response.html', response = reply)
-
-    
-  def get_state_options():
-    listOfStates=[]
+def get_state_options():
+    ListOfStates = []
     with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data) 
-    for county in counties:
-        if not (county["State"]) in listOfStates):
-            listOfStates.append(county["State"])
-            first_state = state ["State"]
-    options = ""
-    for state in listOfStates:
-        options = options + Markup("<option value=\"" + state + "\">" + state + "</State>")
-    return options
+         counties = json.load(demographics_data)
+            for county in counties:
+                if not (county["State"] in ListOfStates):
+                    ListOfStates.append(county["State"])
+                    
+            options = ""
+         for state in ListOfStates:
+            options = options + Markup("<option value=\"" + s + "\">" + s + "</option>")
+        return options
+    
+    
+    if __name__ == '__main__':
         
-if __name__=='__main__':
-    app.run(debug=False)
+        app.run(debug=True)
     
     
     
     
     
     
-    
-    
-    
+  
