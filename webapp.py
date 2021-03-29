8d9461c9754a0df741ef07e56fbe0594848a1610
asal101 <77691090+asal101@users.noreply.github.com>
2021-03-29 09:41:59 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_main():
-    return render_template('home.html')
+    return render_template('home.html', options=get_state_options())
                            
 @app.route("/response")
 def render_response():
