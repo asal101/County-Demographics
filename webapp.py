0942c10c6886e06404d40141bf95238c5c209a24
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-29 09:56:50 -0700

    Create webapp.py

--- webapp.py
+++ webapp.py
@@ -3,35 +3,41 @@ import os
 import json
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('home.html')
 
-@app.route('/funFact')
-def render_fun_facy
-    return render_template('home.html', options=get_state_options(), funFact=fun_fact_by_state(state_chosen)
+def render_fun_fact():
+    state_chosen = request.args['states']
+    
+    
                            
 def get_state_options():
     ListOfStates = []
     with open('county_demographics.json') as demographics_data:
          counties = json.load(demographics_data)
-         for county in counties:
+            for county in counties:
                 if not (county["State"] in ListOfStates):
                     ListOfStates.append(county["State"])
                     
-         options = ""
+            options = ""
          for state in ListOfStates:
             options = options + Markup("<option value=\"" + s + "\">" + s + "</option>")
         return options
     
-def fun_fact_by_state(state):
-    first_county = "ZZZZZZZZ"
+    def fun_fact_by_state(state)
+    first_county = "ZZZZZZZ"
     for county in counties:
-        if county["County"] < first_county and county["State"] == state:
-            first_county = county["County"]
-    return first_county
-
-
+    	if county["County"] < first_county and county[State] == state:
+    		first_county = county["County"]
+    	return first_county
+    
+    
+    
     if __name__ == '__main__':
         
         app.run(debug=True)
+        
+        
+        
+        
