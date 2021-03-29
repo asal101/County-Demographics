df6c50f0c46999259ed2bfed17f3351197f021fb
asal101 <77691090+asal101@users.noreply.github.com>
2021-03-29 09:34:00 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,29 +9,44 @@ def render_main():
                            
 @app.route("/response")
 def render_response():
-    color = request.args['color'] 
+    
+    
+   # color = request.args['color'] 
+    
+    
     #The request object stores information about the request sent to the server.
     #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
     #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
-        reply = "That's my favorite color, too!"
-    else:
-        reply = "My favorite color is pink."
-    return render_template('response.html', response = reply)
     
+    
+   # if color == 'pink':
+   #     reply = "That's my favorite color, too!"
+  #  else:
+  #      reply = "My favorite color is pink."
+  #  return render_template('response.html', response = reply)
+
+    
+  def get_state_options():
+    listOfStates=[]
+    with open('county_demographics.json') as demographics_data:
+        counties = json.load(demographics_data) 
+    for county in counties:
+        if not (county["State"]) in listOfStates):
+            listOfStates.append(county["State"])
+            first_state = state ["State"]
+
+        
+Markup("<State=\"" + s + "\">" + s + "</State>")
+
+
 if __name__=='__main__':
     app.run(debug=False)
-
-   
-  def get_state_options():
-    with open('county_demographics.json') as demographics_data:
-        counties = json.load(demographics_data)    
-        
-      ListOfStates=[]
-        options = options + [0] "State"
-        for state in states:
-        if state["State"]  < first_state:
-            first_state = state ["State"]
-       return first_state     
-
-Markup("<State=\"" + s + "\">" + s + "</State>")
+    
+    
+    
+    
+    
+    
+    
+    
+    
